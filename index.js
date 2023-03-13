516c136747802ba385fce75316d88b234476e98e
Avétis KAZARIAN <avetis.k@gmail.com>
2023-03-13 14:22:39 +0100

    feat: finish all todos routes

--- index.js
+++ index.js
@@ -57,9 +57,56 @@ app.get("/todos/:todoId", async (req, res) => {
 })
 
 // UPDATE
-app.patch("/todos/:todoId", async (req, res) => {})
+app.patch("/todos/:todoId", async (req, res) => {
+  const todoId = Number.parseInt(req.params.todoId, 10)
+  const { description, done } = req.body
+  const db = await read()
+  const {
+    todos: { [todoId]: todo },
+  } = db
+
+  if (!todo) {
+    res.status(404).send({ error: "Not found" })
+
+    return
+  }
+
+  const updatedTodo = {
+    ...todo,
+    description: description ?? todo.description,
+    done: done ?? todo.done,
+  }
+
+  await write(db, {
+    todos: {
+      [todoId]: updatedTodo,
+    },
+  })
+
+  res.send({ result: updatedTodo })
+})
 
 // DELETE
-app.delete("/todos/:todoId", async (req, res) => {})
+app.delete("/todos/:todoId", async (req, res) => {
+  const todoId = Number.parseInt(req.params.todoId, 10)
+  const db = await read()
+  const {
+    todos: { [todoId]: todo },
+  } = db
+
+  if (!todo) {
+    res.status(404).send({ error: "Not found" })
+
+    return
+  }
+
+  await write(db, {
+    todos: {
+      [todoId]: undefined,
+    },
+  })
+
+  res.send({ result: todo })
+})
 
 app.listen(4000, () => console.log("Listening on :4000"))
